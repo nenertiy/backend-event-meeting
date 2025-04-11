24d924b495ccb43b552256c127ae3c94b9acbef7
nenertiy <nenertiy@gmail.com>
2025-04-11 21:19:43 +0300

    add cache for event

--- src/modules/events/events.service.ts
+++ src/modules/events/events.service.ts
@@ -1,6 +1,7 @@
 import {
   ConflictException,
   ForbiddenException,
+  Inject,
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
@@ -12,9 +13,13 @@ import { ParticipantsService } from '../participants/participants.service';
 import { EventStatus } from '@prisma/client';
 import { OrganizersService } from '../organizers/organizers.service';
 import { SearchEventDto } from './dto/search-event.dto';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
+import { Event } from 'src/common/types/event';
 @Injectable()
 export class EventsService {
   constructor(
+    @Inject(CACHE_MANAGER) private readonly cacheManager: Cache,
     private readonly eventsRepository: EventsRepository,
     private readonly mediaService: MediaService,
     private readonly participantsService: ParticipantsService,
@@ -94,15 +99,31 @@ export class EventsService {
   }
 
   async findAll(query?: string, take?: number, skip?: number) {
+    const cachedEvents = await this.cacheManager.get<Event[]>(
+      `events_${query}_${take}_${skip}`,
+    );
+    if (cachedEvents) {
+      return cachedEvents;
+    }
     const events = await this.eventsRepository.findAll(query, take, skip);
     if (events.length === 0) {
       throw new NotFoundException('No events found');
     }
+    const syncedEvents = await this.syncStatuses(events);
 
-    return this.syncStatuses(events);
+    await this.cacheManager.set<Event[]>(
+      `events_${query}_${take}_${skip}`,
+      syncedEvents,
+    );
+
+    return syncedEvents;
   }
 
   async findById(id: string) {
+    const cachedEvent = await this.cacheManager.get<Event>(`event_${id}`);
+    if (cachedEvent) {
+      return cachedEvent;
+    }
     const event = await this.eventsRepository.findById(id);
     if (!event) {
       throw new NotFoundException('Event not found');
@@ -123,34 +144,70 @@ export class EventsService {
       event.status = actualStatus;
     }
 
+    await this.cacheManager.set<Event>(`event_${id}`, event);
+
     return event;
   }
 
   async search(dto: SearchEventDto) {
+    const cachedEvents = await this.cacheManager.get<Event[]>(
+      `events_search_${JSON.stringify(dto)}`,
+    );
+    if (cachedEvents) {
+      return cachedEvents;
+    }
     const events = await this.eventsRepository.search(dto);
     if (events.length === 0) {
       throw new NotFoundException('No events found');
     }
 
-    return this.syncStatuses(events);
+    const syncedEvents = await this.syncStatuses(events);
+
+    await this.cacheManager.set<Event[]>(
+      `events_search_${JSON.stringify(dto)}`,
+      syncedEvents,
+    );
+
+    return syncedEvents;
   }
 
   async findByOrganizerId(organizerId: string) {
+    const cachedEvents = await this.cacheManager.get<Event[]>(
+      `events_organizer_${organizerId}`,
+    );
+    if (cachedEvents) {
+      return cachedEvents;
+    }
     const events = await this.eventsRepository.findByOrganizerId(organizerId);
     if (events.length === 0) {
       throw new NotFoundException('No events found');
     }
+    const syncedEvents = await this.syncStatuses(events);
 
-    return this.syncStatuses(events);
+    await this.cacheManager.set<Event[]>(
+      `events_organizer_${organizerId}`,
+      syncedEvents,
+    );
+
+    return syncedEvents;
   }
 
   async findByTagId(tagId: string) {
+    const cachedEvents = await this.cacheManager.get<Event[]>(
+      `events_tag_${tagId}`,
+    );
+    if (cachedEvents) {
+      return cachedEvents;
+    }
     const events = await this.eventsRepository.findByTagId(tagId);
     if (events.length === 0) {
       throw new NotFoundException('No events found');
     }
+    const syncedEvents = await this.syncStatuses(events);
 
-    return this.syncStatuses(events);
+    await this.cacheManager.set<Event[]>(`events_tag_${tagId}`, syncedEvents);
+
+    return syncedEvents;
   }
 
   async cancelEvent(id: string) {
@@ -158,6 +215,11 @@ export class EventsService {
     if (!event) {
       throw new NotFoundException('Event not found');
     }
+
+    await this.cacheManager.del(`events`);
+    await this.cacheManager.del(`event_${id}`);
+    await this.cacheManager.del(`events_${event.organizerId}`);
+
     return this.eventsRepository.cancelEvent(id);
   }
 
@@ -200,6 +262,10 @@ export class EventsService {
       participantsCount: event.participantsCount + 1,
     });
 
+    await this.cacheManager.del(`events`);
+    await this.cacheManager.del(`event_${eventId}`);
+    await this.cacheManager.del(`events_${event.organizerId}`);
+
     return { message: 'Successfully registered on the event' };
   }
 
@@ -228,6 +294,10 @@ export class EventsService {
     await this.eventsRepository.update(eventId, {
       participantsCount: event.participantsCount - 1,
     });
+
+    await this.cacheManager.del(`events`);
+    await this.cacheManager.del(`event_${eventId}`);
+    await this.cacheManager.del(`events_${event.organizerId}`);
 
     return {
       message: 'You have left the event',
