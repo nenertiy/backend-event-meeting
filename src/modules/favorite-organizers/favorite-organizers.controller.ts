15f9511c5c11b99ee44764b8a9d8f2e2cce3beac
nenertiy <nenertiy@gmail.com>
2025-04-12 01:01:10 +0300

    #fix

--- src/modules/favorite-organizers/favorite-organizers.controller.ts
+++ src/modules/favorite-organizers/favorite-organizers.controller.ts
@@ -6,6 +6,8 @@ import { UserWithoutPassword } from 'src/common/types/user';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @ApiTags('Favorite Organizers')
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard)
 @Controller('favorite-organizers')
 export class FavoriteOrganizersController {
   constructor(
@@ -13,7 +15,6 @@ export class FavoriteOrganizersController {
   ) {}
 
   @Post()
-  @ApiBearerAuth()
   @ApiOperation({ summary: 'Add a favorite organizer' })
   @ApiBody({
     schema: {
@@ -23,7 +24,6 @@ export class FavoriteOrganizersController {
       },
     },
   })
-  @UseGuards(JwtAuthGuard)
   async addFavoriteOrganizer(
     @DecodeUser() user: UserWithoutPassword,
     @Body() body: { organizerId: string },
@@ -35,7 +35,6 @@ export class FavoriteOrganizersController {
   }
 
   @Delete()
-  @ApiBearerAuth()
   @ApiOperation({ summary: 'Remove a favorite organizer' })
   @ApiBody({
     schema: {
@@ -45,7 +44,6 @@ export class FavoriteOrganizersController {
       },
     },
   })
-  @UseGuards(JwtAuthGuard)
   async removeFavoriteOrganizer(
     @DecodeUser() user: UserWithoutPassword,
     @Body() body: { organizerId: string },
@@ -57,9 +55,7 @@ export class FavoriteOrganizersController {
   }
 
   @Get()
-  @ApiBearerAuth()
   @ApiOperation({ summary: 'Get all favorite organizers' })
-  @UseGuards(JwtAuthGuard)
   async getFavoriteOrganizers(@DecodeUser() user: UserWithoutPassword) {
     return this.favoriteOrganizersService.getUserFavoriteOrganizers(user.id);
   }
